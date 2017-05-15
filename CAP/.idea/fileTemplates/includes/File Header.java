fdae31f4ca0b5ceaeae4223fd56badc5145c64c8
Tolerably <williams.eric.thomas@gmail.com>
2017-05-15 09:55:58 -0700

    Auto fill names
    Changed "File Header.java" to make the author names  appear when
    creating new files.

---
+++ CAP/.idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,4 @@
+#set( $USER = "Vikas A., Tudor C., Michael S., Eric W." )
+/**
+ * Created by ${USER} on ${DATE}.
+ */
--- CAP/app/src/main/java/org/pltw/examples/cap/MainActivity.java
+++ CAP/app/src/main/java/org/pltw/examples/cap/MainActivity.java
@@ -3,6 +3,11 @@ package org.pltw.examples.cap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+ * Project created by Vikas A., Tudor C., Michael S., Eric W.
+ * Client is Patrick Williams
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
